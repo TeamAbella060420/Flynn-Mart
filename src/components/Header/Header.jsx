b0bac917b0a1f182c2ecc3929fc89806ebc237ea
Muhandis <Muhandis@wf.local>
2023-02-22 21:02:15 +0800

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -56,9 +56,14 @@ const Header = () => {
                             </div>
 
                             <div className="nav__icons">
-                                <span className="fav__icon"><i class="ri-heart-line"></i></span>
+                                <span className="fav__icon">
+                                    <i class="ri-heart-line"></i>
+                                    <span className="badge">1</span>
+                                </span>
                                 <span className="cart__icon"><i 
-                                    class="ri-shopping-bag-line"></i></span>
+                                    class="ri-shopping-bag-line"></i>
+                                    <span className="badge">1</span>
+                                </span>
                                 <span>
                                     <motion.img whileTap={{ scale: 1.2}} src={userIcon} alt="" />
                                 </span>
