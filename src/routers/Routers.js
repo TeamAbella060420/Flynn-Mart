4ad0797f8aacf7c8c63457133079a699b7df7ea9
Muhandis <Muhandis@wf.local>
2023-02-22 15:56:37 +0800

    Update Routers.js

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,4 +1,12 @@
 import React from "react";
+import Cart from "../pages/Cart";
+import Checkout from "../pages/Checkout";
+import Home from "../pages/home";
+import Login from "../pages/Login";
+import ProductDetails from "../pages/ProductDetails";
+import Products from "../pages/Products";
+import Shop from "../pages/Shop";
+import Signup from "../pages/Signup";
 
 const Routers = () => {
     return (
