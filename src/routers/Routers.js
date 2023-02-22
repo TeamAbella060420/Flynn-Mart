b37057f84fa40e092a9c6c196f9269712dc51ac7
Muhandis <Muhandis@wf.local>
2023-02-22 16:03:18 +0800

    Update Routers.js

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -1,4 +1,5 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Cart from "../pages/Cart";
 import Checkout from "../pages/Checkout";
 import Home from "../pages/home";
@@ -10,7 +11,15 @@ import Signup from "../pages/Signup";
 
 const Routers = () => {
     return (
-        <div>Router</div>
+        <Routes>
+            <Route path="home" element={<Home/>}/>
+            <Route path="shop" element={<Shop/>}/>
+            <Route path="shop/:id" element={<ProductDetails/>}/>
+            <Route path="cart" element={<Cart/>}/>
+            <Route path="checkout" element={<Checkout/>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="signup" element={<Signup/>}/>
+        </Routes>
     )
 }
 
