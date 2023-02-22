8ea88380572ed7eb1b03da388434e9573c8e9c7b
Muhandis <Muhandis@wf.local>
2023-02-22 17:31:30 +0800

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import './header.css'
+import { Container, Row } from "reactstrap";
+
+import logo from '../../assets/images/eco-logo.png'
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return <div>Header</div>
+    return (
+        <header className="header">
+            <Container>
+                <Row>
+                    <div className="nav__wrapper">
+                        <div className="logo">
+                            <img src={logo} alt="logo" />
+                            <div>
+                                <h1>Flynn-Mart</h1>
+                                <p>Since 2023</p>
+                            </div>
+                        </div>
+                            <div className="navigation">
+                                <ul className="menu">
+                                    <li className="nav__item">
+                                        <NavLink to='home'>Home</NavLink>
+                                    </li>
+                                    <li className="nav__item">
+                                        <NavLink to='home'>Shop</NavLink>
+                                    </li>
+                                    <li className="nav__item">
+                                        <NavLink to='home'>Cart</NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div className="nav__icons">
+                                <span className="cart__icon"><i class="ri-shopping-bag-line"></i></span>
+                            </div>
+                    </div>
+                </Row>
+            </Container>
+        </header>
+    )
 };
 
 export default Header;
