0b4ff366c324918798abeac7fd1f28428979766f
Muhandis <Muhandis@wf.local>
2023-02-23 15:40:18 +0800

    Create ProductCard.jsx

---
+++ src/components/UI/ProductCard.jsx
@@ -0,0 +1,27 @@
+import { motion } from "framer-motion";
+import React from "react";
+import productImg from '../../assets/images/arm-chair-01.jpg'
+import '../../styles/product-card.css'
+import { Container, Col, Row } from "reactstrap";
+
+const ProductCard = () => {
+    return (
+        <>
+        <Col lg='3' md='8'>
+            <div className="product__item">
+                <div className="product__img">
+                    <img src={productImg} alt="" />
+                </div>
+                <h3 className="product__name">Modern Armchair</h3>
+                <span>Chair</span>
+                <div className="product__card-bottom">
+                    <span className="price">$299</span>
+                    <span><i class="ri-add-box-line"></i></span>
+                </div>
+            </div>
+        </Col>
+        </>
+    )
+};
+
+export default ProductCard;
