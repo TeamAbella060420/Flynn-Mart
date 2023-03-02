ecc51eb4adc602516890eb370e3633c1ecb98f16
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2023-03-02 17:31:55 +0800

    Update home.jsx

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -65,7 +65,7 @@ const Home = () => {
 
                         <Col lg='6' md='6'>
                             <div className="hero__img">
-                                <img src={heroImg} alt="" />
+                                <img src={heroImg} alt="" /> // This is the image of a chair located in the center of the Home Page
                             </div>
                         </Col>
                     </Row>
