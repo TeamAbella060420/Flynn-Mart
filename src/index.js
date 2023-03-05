f7904f576712b68940dc360856986bf1f0851910
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2023-03-05 17:54:51 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,12 +7,24 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import store from './redux/store'
 import { Provider } from 'react-redux'
+import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* Same as */}
+      <ToastContainer />
         <App />
       </Provider>
     </BrowserRouter>
