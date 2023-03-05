e647e6ff33d793b234db2029de235dbc0a2da35c
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2023-03-05 18:46:31 +0800

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from '../components/Helmet/Helmet'
+import { Container,Row, Col } from "reactstrap";
 
 const Shop = () => {
     return (
         <Helmet title='Shop'>
             <CommonSection title='Products'/>
+
+            <section>
+                
+            </section>
         </Helmet>
     )
 };
