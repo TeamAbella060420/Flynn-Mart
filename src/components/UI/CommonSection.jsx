e4f5fa813c6d31dd8474e628b9c84781d3e68f90
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2023-03-05 18:14:23 +0800

    Create CommonSection.jsx

---
+++ src/components/UI/CommonSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container } from 'reactstrap'
+
+
+const CommonSection = () => {
+  return (
+    <section className="common__section">
+        <Container>
+            <h1></h1>
+        </Container>
+    </section>
+  )
+}
+
+export default CommonSection
