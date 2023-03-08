82c2d77eadb5b7db42da6fd68f5b0614bd8a937c
TeamAbella060420 <96814164+TeamAbella060420@users.noreply.github.com>
2023-03-08 09:19:10 +0800

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import CommonSection from "../components/UI/CommonSection";
 import Helmet from '../components/Helmet/Helmet'
 import { Container,Row, Col } from "reactstrap";
 
 import '../styles/shop.css'
+import products from "../assets/data/products";
+import ProductsList from "../components/UI/ProductsList";
 
 const Shop = () => {
+    const [productsData, setProductData] = useState(products);
+    const handleFilter = e => {
+        const filterValue = e.target.value
+        if(filterValue === 'sofa') {
+            const filteredProducts = products.filter(item => item.category === 'sofa')
+            setProductData(filteredProducts)
+        }
+    }
+
     return (
         <Helmet title='Shop'>
             <CommonSection title='Products'/>
@@ -15,7 +26,7 @@ const Shop = () => {
                     <Row>
                         <Col lg='3' md='3'>
                             <div className="filter__widget">
-                            <select>
+                            <select onChange={handleFilter}>
                                 <option>Filter By Category</option>
                                 <option value="sofa">Sofa</option>
                                 <option value="mobile">Mobile</option>
@@ -45,6 +56,18 @@ const Shop = () => {
                     </Row>
                 </Container>
             </section>
+
+            <section>
+                <Container>
+                    <Row>
+                    {
+                        productsData.length === 0 ? <h1>No products are found!</h1>
+                        : <ProductsList data={productsData} />
+                    }
+                    </Row>
+                </Container>
+            </section>
+
         </Helmet>
     )
 };
