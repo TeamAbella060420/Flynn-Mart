45aa204fcbf77916d4b3f327b61445644a6d254e
Muhandis <Muhandis@wf.local>
2023-03-09 16:52:19 +0800

    Update ProductDetails.jsx

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
+import products from "../assets/data/products";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
 
 const ProductDetails = () => {
-    return <div>ProductDetails</div>
+
+    const {id} = useParams()
+    const product = products.find(item => item.id === id)
+
+    const { imgUrl, productName, price, avgRating, review, description} = product
+
+    return (
+        <Helmet>
+            <CommonSection />
+
+            <section className="pt-0">
+                <Container>
+                    <Row>
+                        <Col lg='6'>
+                            <img src={imgUrl} alt="" />
+                        </Col>
+                        <Col lg='6'>
+                            <div className="product__details">
+                                <h2>
+                                    productName
+                                </h2>
+                                <div className="product__rating">
+                                    <div>
+                                        <span><i class="ri-star-s-fill"></i></span>
+                                        <span><i class="ri-star-s-fill"></i></span>
+                                        <span><i class="ri-star-s-fill"></i></span>
+                                        <span><i class="ri-star-s-fill"></i></span>
+                                        <span><i class="ri-star-half-s-line"></i></span>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
+    )
 };
 
 export default ProductDetails;
