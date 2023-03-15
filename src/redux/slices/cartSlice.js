e743a34da5704275f041d0533cb95e43fa4cf76f
Muhandis <Muhandis@wf.local>
2023-03-15 21:10:50 +0800

    Update cartSlice.js

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -25,7 +25,7 @@ const cartSlice = createSlice({
             state.cartItems.push({
                 id: newItem.id,
                 productName: newItem.productName,
-                image: newItem.imgUrl,
+                imgUrl: newItem.imgUrl,
                 price: newItem.price,
                 quantity: 1,
                 totalPrice: newItem.price
@@ -41,7 +41,20 @@ const cartSlice = createSlice({
         state.totalAmount = state.cartItems.reduce((total, item) => total+
         Number(item.price) * Number(item.quantity))
 
+    },
+  },
+
+  deleteItem:(state, action) => {
+    const id = action.payload
+    const existingItem = state.cartItems.find(item => item.id === id)
+
+    if(existingItem) {
+        state.cartItems = state.cartItems.filter(item => item.id === id)
+        state.totalQuantity = state.totalQuantity - existingItem.quantity
     }
+
+    state.totalAmount = state.cartItems.reduce((total, item) => total+
+    Number(item.price) * Number(item.quantity))
   }
 });
 
