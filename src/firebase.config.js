cb3ad8fd119589ddec8d450e2eac5b67ff6ced35
Muhandis <Muhandis@wf.local>
2023-03-16 21:02:25 +0800

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,11 +1,7 @@
 
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyD7lkjqO_1LdxJuvMbL5jmLYPNNkI2bQQU",
   authDomain: "flyynmart.firebaseapp.com",
