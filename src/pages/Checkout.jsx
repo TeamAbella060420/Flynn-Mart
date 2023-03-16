bb694d7798197c424e58ad7fc8a1b9a2839e4ec4
Muhandis <Muhandis@wf.local>
2023-03-16 15:56:53 +0800

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import Helmet from '../components/Helmet/Helmet'
+import CommonSection from '../components/UI/CommonSection'
 
 const Checkout = () => {
     return (
-        <div>Checkout</div>
+        <Helmet title='Checkout'>
+            <CommonSection />
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg='8'>
+                            <h6 className="mb-4 fw-bold">Billing Information</h6>
+                            <Form>
+                                <FormGroup className="form__group"></FormGroup>
+                            </Form>
+                        </Col>
+                        <Col lg='4'>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
     )
 };
 
