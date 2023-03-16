1cff13c299096e0d1db1f4220bfbed426006b531
Muhandis <Muhandis@wf.local>
2023-03-16 21:01:02 +0800

    Create firebase.config.js

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD7lkjqO_1LdxJuvMbL5jmLYPNNkI2bQQU",
+  authDomain: "flyynmart.firebaseapp.com",
+  projectId: "flyynmart",
+  storageBucket: "flyynmart.appspot.com",
+  messagingSenderId: "493347908675",
+  appId: "1:493347908675:web:83bd44ea932aecbfea38f6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
