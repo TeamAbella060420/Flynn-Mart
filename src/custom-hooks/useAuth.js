9b849a86fce6b3d349c4ea20d151628009d5385e
Muhandis <Muhandis@wf.local>
2023-03-18 13:33:33 +0800

    Update useAuth.js

--- src/custom-hooks/useAuth.js
+++ src/custom-hooks/useAuth.js
@@ -1,14 +1,26 @@
 
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../firebase.config'
 
 const useAuth = () => {
 
-    const [current , setCurrentUser] = useState({})
+    const [currentUser , setCurrentUser] = useState({})
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if(user) {
+                setCurrentUser(user)
+            } else {
+                setCurrentUser(null)
+            }
+        })
+    })
         return (
-            <div>useAuth</div>
+            {
+                currentUser,
+            }
         )
 }
 
-export default useAuth
+export default useAuth;
