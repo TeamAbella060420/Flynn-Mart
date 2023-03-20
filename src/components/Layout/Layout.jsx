020220b06a9f3ba0de49c064fae73a621665f1eb
Muhandis <Muhandis@wf.local>
2023-03-20 15:14:00 +0800

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,9 +3,18 @@ import Routers from "../../routers/Routers";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
+import AdminNav from "../Admin/AdminNav";
+import { useLocation } from "react-router-dom";
+
 const Layout = () => {
+
+    const Location = useLocation()
+
     return (
         <>
+        {
+            location.pathname.startsWith('/dashboard')
+        }
             <Header/>
                 <div>
                     <Routers/>
