d70ceab85a9c4ae117772f7317e9109be56a7040
Muhandis <Muhandis@wf.local>
2023-03-20 16:20:53 +0800

    Update AdminNav.jsx

--- src/components/Admin/AdminNav.jsx
+++ src/components/Admin/AdminNav.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import Header from '../Header/Header';
 
 const AdminNav = () => {
   return (
-    <div>AdminNav</div>
+    <Header className='admin__header'>
+        <div className="admin__nav-top">
+            <Container>
+                <div className='admin__nav-wrapper-top'>
+                    <div className="logo">
+                        <h2>FlyNNMart</h2>
+                    </div>
+
+                    <div className="search__box">
+                        <input type="text" placeholder='Search....'/>
+                        <span></span>
+                    </div>
+                </div >
+            </Container>
+        </div>
+    </Header>
   )
 }
 
