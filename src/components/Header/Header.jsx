3c6a9a6387347c57be80e226ae188848dfabc39b
Muhandis <Muhandis@wf.local>
2023-03-20 15:13:58 +0800

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -145,6 +145,7 @@ const Header = () => { // create Header component function
                             <div className="d-flex align-items-center justify-content-center flex-column">
                                 <Link to='/signup'>Signup</Link>
                                 <Link to='/login'>Login</Link>
+                                <Link to='/dashboard'>Dashboard</Link>
                             </div>
                         }
                     </div>
