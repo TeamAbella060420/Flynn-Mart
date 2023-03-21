9d104e1b17e3b2f777c3285ea241de65736ebf15
Muhandis <Muhandis@wf.local>
2023-03-21 13:46:20 +0800

    Update AdminNav.jsx

--- src/components/Admin/AdminNav.jsx
+++ src/components/Admin/AdminNav.jsx
@@ -62,7 +62,7 @@ const AdminNav = () => {
                         {
                             admin__nav.map((item, index) => (
                               <li className="admin__menu-item" key={index}>
-                                 <NavLink to={item.path}>{item.display}</NavLink>
+                                 <NavLink to={item.path} className={navClass => navClass.isActive ? 'acitve__admin-menu' : ''}>{item.display}</NavLink>
                               </li>
                             ))
                         }
