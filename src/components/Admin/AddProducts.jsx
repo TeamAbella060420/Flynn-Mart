e06bb04153600119b31132dd791790f3919d95d6
Muhandis <Muhandis@wf.local>
2023-03-21 16:36:35 +0800

    Update AddProducts.jsx

--- src/components/Admin/AddProducts.jsx
+++ src/components/Admin/AddProducts.jsx
@@ -7,8 +7,12 @@ const AddProducts = () => {
   const [enterShortDesc, setEnterShortDesc] = useState('');
   const [enterShortDescription, setEnterShortDescription] = useState('');
   const [enterCategory, setEnterCategory] = useState('');
-  const [enterEnterPrice, setEnterEnterPrice] = useState('');
+  const [enterPrice, setEnterPrice] = useState('');
   const [enterProductImg, setEnterProductImg] = useState(null);
+
+  const addProducts = async (e) => {
+    e.preventDefault()
+  }
 
   return (
     <section>
@@ -19,25 +23,25 @@ const AddProducts = () => {
             <Form>
               <FormGroup className="form__group">
                 <span>Product title</span>
-                <input type="text" placeholder='Double Sofa' value={enterTitle} onChange={ e => e.target.value}/>
+                <input type="text" placeholder='Double Sofa' value={enterTitle} onChange={e => setEnterTitle(e.target.value)} required />
               </FormGroup>
               <FormGroup className="form__group">
                 <span>Short Description</span>
-                <input type="text" placeholder='lorem....' value={enterTitle} onChange={ e => e.target.value}/>
+                <input type="text" placeholder='lorem....' value={enterShortDesc} onChange={e => setEnterShortDesc(e.target.value)} required />
               </FormGroup>
               <FormGroup className="form__group">
                 <span>Description</span>
-                <input type="text" placeholder='Description....' />
+                <input type="text" placeholder='Description....' value={enterShortDescription} onChange={e => setEnterShortDescription(e.target.value)} required />
               </FormGroup>
 
               <div className='d-flex align-items-center justify-content-between gap-5'>
                 <FormGroup className="form__group w-50">
                   <span>Price</span>
-                  <input type="number" placeholder='$100' />
+                    <input type="number" placeholder='$100' value={enterPrice} onChange={e => setEnterPrice(e.target.value)} required />
                 </FormGroup>
                 <FormGroup className="form__group w-50">
                   <span>Category</span>
-                  <select className='w-100 p-2'>
+                  <select className='w-100 p-2' value={enterCategory} onChange={e => setEnterCategory(e.target.value)} required>
                     <option value="chair">Chair</option>
                     <option value="sofa">Sofa</option>
                     <option value="mobile">Mobile</option>
@@ -50,7 +54,7 @@ const AddProducts = () => {
               <div>
                 <FormGroup className="form__group">
                   <span>Product Image</span>
-                  <input type="file" />
+                  <input type="file" required />
                 </FormGroup>
               </div>
 
